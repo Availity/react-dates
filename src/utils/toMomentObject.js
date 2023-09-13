8fe8e1852859a4522dcc66dc4e8e5145404e1f15
Greg Martin <greg.martin@availity.com>
2023-09-13 12:51:33 -0400

    refactor: add MMDDYYYY and YYYYMMDD to toMomentObject

--- src/utils/toMomentObject.js
+++ src/utils/toMomentObject.js
@@ -4,8 +4,8 @@ import { DISPLAY_FORMAT, ISO_FORMAT } from '../constants';
 
 export default function toMomentObject(dateString, customFormat) {
   const dateFormats = customFormat
-    ? [customFormat, DISPLAY_FORMAT, ISO_FORMAT]
-    : [DISPLAY_FORMAT, ISO_FORMAT];
+    ? [customFormat, DISPLAY_FORMAT, ISO_FORMAT, 'MMDDYYYY', 'YYYYMMDD']
+    : [DISPLAY_FORMAT, ISO_FORMAT, 'MMDDYYYY', 'YYYYMMDD'];
 
   const date = moment(dateString, dateFormats, true);
   return date.isValid() ? date.hour(12) : null;
